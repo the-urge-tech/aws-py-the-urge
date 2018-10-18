8a1597445fbd0cf50d81497095fe1fcdd0c06520
casertap <pierre.caserta@gmail.com>
2018-10-18 15:14:54 +1100

    extract key_id for parsed and enriched

--- aws_py_the_urge/app/__init__.py
+++ aws_py_the_urge/app/__init__.py
@@ -4,16 +4,32 @@ __version__ = '0.1.0'
 __author__ = 'the urge <pierre.caserta@gmail.com>'
 __all__ = []
 
+import re
 import urllib.parse
 from typing import NamedTuple, Text
 
 EventRecordBase = NamedTuple("EventRecordBase",
                              [("object_key", Text), ("event_name", Text),
-                              ("bucket_name", Text)])
+                              ("bucket_name", Text), ("key_id", Text),
+                              ("retailer_code", Text)])
 
 
+# TODO not a good usage of namedtuple
+# this should be a normal class
 class EventRecord(EventRecordBase):
     def __new__(cls, **kargs):
-        kargs['object_key'] = urllib.parse.unquote(kargs['object_key'])
+        object_key = urllib.parse.unquote(kargs['object_key'])
+        kargs['object_key'] = object_key
+        matches = re.search('.*?\/retailer_code=(.*?)\/.*', object_key)
+        kargs['retailer_code'] = matches.group(1)
+        matches = re.search('.*?--fetched--(.*?)\..*', object_key)
+        if matches:
+            kargs['key_id'] = matches.group(1)
+        matches = re.search('.*?--parsed--(.*?)\..*', object_key)
+        if matches:
+            kargs['key_id'] = matches.group(1)
+        matches = re.search('.*?--enriched--(.*?)\..*', object_key)
+        if matches:
+            kargs['key_id'] = matches.group(1)
         self = super(EventRecord, cls).__new__(cls, **kargs)
         return self
--- tests/event_record_test.py
+++ tests/event_record_test.py
@@ -8,7 +8,7 @@ class EventRecordTests(unittest.TestCase):
     def setUpClass(cls):
         super(EventRecordTests, cls).setUpClass()
 
-    def test_all_unquote(self):
+    def test_event_record_create(self):
         er = EventRecord(
             bucket_name="test1",
             object_key=
@@ -19,3 +19,32 @@ class EventRecordTests(unittest.TestCase):
             er.object_key,
             "type=fetched/retailer_code=au-ssense/year=2018/month=10/day=5/crawl_id=20181005212630__au-ssense/20181005212630__au-ssense--fetched--j9icejmp.jl.gz"
         )
+        self.assertEqual(er.key_id, "j9icejmp")
+        self.assertEqual(er.retailer_code, "au-ssense")
+
+    def test_extract_key_id_fetched(self):
+        er = EventRecord(
+            bucket_name="test1",
+            object_key=
+            "type%3Dfetched/retailer_code%3Dau-ssense/.../20181005212630__au-ssense--fetched--j9icejmp.jl.gz",
+            event_name="ObjectCreated:Put")
+
+        self.assertEqual(er.key_id, "j9icejmp")
+
+    def test_extract_key_id_parsed(self):
+        er = EventRecord(
+            bucket_name="test1",
+            object_key=
+            "type%3Dparsed/retailer_code%3Dau-ssense/.../20181005212630__au-ssense--parsed--j9icejmp.jl.gz",
+            event_name="ObjectCreated:Put")
+
+        self.assertEqual(er.key_id, "j9icejmp")
+
+    def test_extract_key_id_enriched(self):
+        er = EventRecord(
+            bucket_name="test1",
+            object_key=
+            "type%3Denriched/retailer_code%3Dau-ssense/.../20181005212630__au-ssense--enriched--j9icejmp.jl.gz",
+            event_name="ObjectCreated:Put")
+
+        self.assertEqual(er.key_id, "j9icejmp")
