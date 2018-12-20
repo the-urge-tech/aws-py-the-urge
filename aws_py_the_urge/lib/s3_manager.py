5445cfbe8fd000b47b7d65c711f10e81cd3d792c
rachid <azi92rach@gmail.com>
2018-12-20 14:59:26 +1100

    create new classes

--- aws_py_the_urge/lib/s3_manager.py
+++ aws_py_the_urge/lib/s3_manager.py
@@ -1,12 +1,10 @@
 import logging
-import re
-from datetime import date
+import os
 from pathlib import Path
 from typing import Any, NamedTuple, Text
 
 import boto3
-
-from aws_py_the_urge.util.date import path_date_extractor
+from botocore.config import Config
 
 LOG = logging.getLogger(__name__)
 
@@ -15,40 +13,29 @@ S3Object = NamedTuple("S3Object", [("obj", Any), ("path", Text),
 
 
 class S3Manager(object):
-    def __init__(
-            self,
-            bucket_name,
-            retailer_code,
-    ):
-        self._retailer_code = retailer_code
-        self._s3 = boto3.resource('s3')
+    def __init__(self, bucket_name, aws_region='ap-southeast-2'):
         self._bucket_name = bucket_name
-        self._bucket = self._s3.Bucket(self._bucket_name)
+        self._s3_client = boto3.client(
+            's3', aws_region, config=Config(s3={'addressing_style': 'path'}))
+        self._s3_resource = boto3.resource(
+            's3', aws_region, config=Config(s3={'addressing_style': 'path'}))
+        self._bucket = self._s3_resource.Bucket(self._bucket_name)
 
-    def find_newest_feed(self):
-        newest_date = date(1, 1, 1)
-        newest_obj = None
-        newest_path = None
-        newest_filename = None
-        for obj in self._bucket.objects.filter(
-                Prefix="format=original/retailer_code={}".format(
-                    self._retailer_code)):
-            LOG.debug(obj)
-            current_date = path_date_extractor(obj.key)
-            if current_date > newest_date:
-                newest_date = current_date
-                newest_obj = obj
-                newest_matches = re.search(
-                    '.*?\/(year=.*?\/month=.*?\/day=.*?)\/(.*)', obj.key)
-                newest_path = newest_matches.group(1)
-                newest_filename = newest_matches.group(2)
-        return S3Object(
-            obj=newest_obj, path=newest_path, filename=newest_filename)
+    def download(self, key, local_path, file_name):
+        # if not Path(local_output).exists():
+        Path(local_path).mkdir(parents=True, exist_ok=True)
+        # self._s3.Object(self._bucket_name,
+        #                          key).download_file(local_output)
+        local_file_output = '{}/{}'.format(local_path, file_name)
+        LOG.info("Downloading S3: {}/{} to Local: {}".format(
+            self._bucket_name, key, local_file_output))
+        self._s3_client.download_file(self._bucket_name, key,
+                                      local_file_output)
+        LOG.info("Download: {}".format(local_file_output))
+        return local_file_output
 
-    def download(self, key, local_output_path, local_output):
-        if not Path(local_output).exists():
-            Path(local_output_path).mkdir(parents=True, exist_ok=True)
-            # TODO if does not exists
-            LOG.debug("Downloading S3: {} to Local: {}".format(
-                key, local_output))
-            self._s3.Object(self._bucket_name, key).download_file(local_output)
+    def upload(self, key, local_prefix):
+        LOG.info("Uploading Local: {} to S3: {}/{}".format(
+            local_prefix, self._bucket_name, key))
+        self._s3_client.upload_file(local_prefix, self._bucket_name, key)
+        return local_prefix
