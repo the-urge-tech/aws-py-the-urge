97fdef453ec1ac1d1b634fbf68df6193ea87decd
rachid <azi92rach@gmail.com>
2019-01-11 21:58:44 +1100

    improve structure s3_manager and feed_manager

--- aws_py_the_urge/lib/feed_manager.py
+++ aws_py_the_urge/lib/feed_manager.py
@@ -1,17 +1,11 @@
 import gzip
 import logging
-from typing import Any, NamedTuple, Text
 
 from aws_py_the_urge.lib.local_file_manager import LocalFileManager
-from aws_py_the_urge.lib.s3_manager import S3Manager
-from aws_py_the_urge.util.date import get_newest_file
-from aws_py_the_urge.util.path_manager import split_path
+from aws_py_the_urge.lib.s3_manager import S3Manager, S3Object
 from aws_py_the_urge.settings import FILE_EXTENSION
 
 LOG = logging.getLogger(__name__)
-
-S3Object = NamedTuple("S3Object", [("obj", Any), ("path", Text),
-                                   ("filename", Text)])
 
 
 class FeedManager(S3Manager):
@@ -20,44 +14,13 @@ class FeedManager(S3Manager):
         super(FeedManager, self).__init__(bucket_name, aws_region)
         self._retailer_code = retailer_code
 
-    def find_newest_feed(self):
+    def find_last_feed(self):
         prefix = "format=original/retailer_code={}".format(self._retailer_code)
-        list_objects = self.get_list_all_files(prefix=prefix)
-        LOG.debug("list_objects:{}".format(list_objects))
-        if not list_objects:
-            LOG.warning("No files found in {}".format(prefix))
-            return []
-
-        prefix_newest_obj = get_newest_file(list_objects, FILE_EXTENSION)
-        LOG.debug("prefix_newest_obj:{}".format(prefix_newest_obj))
-        if not prefix_newest_obj:
-            LOG.error(
-                "The s3 path list does not contain any file with extension {}. List: {}"
-                .format(FILE_EXTENSION, list_objects))
-            return []
-
-        newest_obj = self._s3_resource.Object(self._bucket_name,
-                                              prefix_newest_obj)
-        LOG.debug("newest_obj:{}".format(newest_obj))
-
-        newest_path, newest_filename = split_path(prefix_newest_obj)
-        s3_object = S3Object(
-            obj=newest_obj, path=newest_path, filename=newest_filename)
-        LOG.debug("S3Object: {}".format(s3_object))
-
-        return s3_object
-
-    def put(self, output, body):
-        LOG.debug("Put in {}/{}".format(self._bucket_name, output))
-        self._s3_resource.Object(self._bucket_name, output).put(Body=body)
-
-    def is_equal_to_last(self, new_feed_gz):
-        last_feed_content = self.get_last_feed_content()
-        new_feed = gzip.decompress(new_feed_gz)
-        return new_feed == last_feed_content
+        newest_feed = self.find_last_obj(prefix, FILE_EXTENSION)
+        return newest_feed
 
     def get_last_feed_content(self):
-        newest_s3_object = self.find_newest_feed()
+        newest_s3_object = S3Object(self.find_last_feed())
         if not newest_s3_object:
             return []
         local_feed_output_path = "/tmp/feedsldtos3/{}".format(
@@ -70,3 +33,11 @@ class FeedManager(S3Manager):
         self.download(newest_s3_object.obj.key, local_feed_output_path,
                       newest_s3_object.filename)
         return LocalFileManager(local_feed_output_file).get_feed_content()
+
+    def put(self, path, body):
+        self.put_into_s3_object(path, body)
+
+    def is_equal_to_last(self, new_feed_gz):
+        last_feed_content = self.get_last_feed_content()
+        new_feed = gzip.decompress(new_feed_gz)
+        return new_feed == last_feed_content
--- aws_py_the_urge/lib/s3_manager.py
+++ aws_py_the_urge/lib/s3_manager.py
@@ -1,50 +1,21 @@
 import logging
-from pathlib import Path
-import os
+from typing import Any, NamedTuple, Text
 
-import boto3
-from botocore.config import Config
+from aws_py_the_urge.lib.s3_parent import S3Parent
+from aws_py_the_urge.util.path_manager import split_path
+from aws_py_the_urge.util.date import get_newest_file
 
 LOG = logging.getLogger(__name__)
 
+S3Object = NamedTuple("S3Object", [("obj", Any), ("size", int), ("path", Text),
+                                   ("filename", Text)])
 
-class S3Manager(object):
+
+class S3Manager(S3Parent):
     def __init__(self, bucket_name, aws_region='ap-southeast-2'):
-        self._bucket_name = bucket_name
-        self._s3_client = boto3.client(
-            's3', aws_region, config=Config(s3={'addressing_style': 'path'}))
-        self._s3_resource = boto3.resource(
-            's3', aws_region, config=Config(s3={'addressing_style': 'path'}))
-        self._bucket = self._s3_resource.Bucket(self._bucket_name)
+        super(S3Manager, self).__init__(bucket_name, aws_region)
 
-    def download(self, key, local_path, file_name):
-        # local_dir_path = os.path.dirname("{}/{}".format(local_path, key))
-        if not Path(local_path).exists():
-            Path(local_path).mkdir(parents=True, exist_ok=True)
-        local_file_output = '{}/{}'.format(local_path, file_name)
-        LOG.debug("Downloading S3: {}/{} to Local: {}".format(
-            self._bucket_name, key, local_file_output))
-        self._s3_client.download_file(self._bucket_name, key,
-                                      local_file_output)
-        LOG.debug("Downloaded in: {}".format(local_file_output))
-        return local_file_output
-
-    def upload(self, key, local_file):
-        LOG.debug("Uploading Local: {} to S3: {}/{}".format(
-            local_file, self._bucket_name, key))
-        self._s3_client.upload_file(local_file, self._bucket_name, key)
-        return local_file
-
-    def get_list_all_files(self, prefix):
-        list_objects = self._s3_client.list_objects_v2(
-            Bucket=self._bucket_name, Prefix=prefix)
-        list_path_files = [
-            file['Key'] for file in list_objects.get('Contents', [])
-        ]
-        return list_path_files
-
-    def get_list_files_contain(self, prefix, name_file_expected):
-        assert type(name_file_expected) is list
+    def get_list_files_contain(self, prefix, name_file_expected: list):
         matching_paths = []
         list_path_files = self.get_list_all_files(prefix)
         for name_expected in name_file_expected:
@@ -53,5 +24,49 @@ class S3Manager(object):
             ]
         return matching_paths
 
-    def exists(self, prefix):
-        return len(self.get_list_all_files(prefix)) > 0
+    def get_s3_object(self, prefix):
+        s3_object_received = self._s3_resource.Object(self._bucket_name,
+                                                      prefix)
+        LOG.debug("obj:{}".format(s3_object_received))
+
+        size = S3Manager.__get_size_s3_object(s3_object_received)
+        newest_path, newest_filename = split_path(prefix)
+
+        s3_object = S3Object(
+            obj=s3_object_received,
+            size=size,
+            path=newest_path,
+            filename=newest_filename)
+        LOG.debug("S3Object: {}".format(s3_object))
+
+        return s3_object
+
+    def put_into_s3_object(self, path, body):
+        LOG.debug("Put in {}/{}".format(self._bucket_name, path))
+        self._s3_resource.Object(self._bucket_name, path).put(Body=body)
+
+    def find_last_obj(self, prefix, file_extension):
+        list_objects = self.get_list_all_files(prefix=prefix)
+        LOG.debug("list_objects:{}".format(list_objects))
+        if not list_objects:
+            LOG.warning("No files found in {}".format(prefix))
+            return []
+
+        prefix_newest_obj = get_newest_file(list_objects, file_extension)
+        LOG.debug("prefix_newest_obj:{}".format(prefix_newest_obj))
+        if not prefix_newest_obj:
+            LOG.error(
+                "The s3 path list does not contain any file with extension {}. List: {}"
+                .format(file_extension, list_objects))
+            return []
+        return self.get_s3_object(prefix_newest_obj)
+
+    @staticmethod
+    def __get_size_s3_object(s3_object):
+        try:
+            size = s3_object.content_lengt
+            LOG.debug("s3_object size:{}".format(size))
+            return size
+        except Exception as e:
+            LOG.exception(e)
+            return 0
