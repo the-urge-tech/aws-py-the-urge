6a6aa5220dfc35ff7731733aad32630c9e56a524
rachid <azi92rach@gmail.com>
2019-01-18 15:22:04 +1100

    add split_name method

--- aws_py_the_urge/util/path_manager.py
+++ aws_py_the_urge/util/path_manager.py
@@ -11,6 +11,11 @@ def split_path(path: str):
     return directory, file_name
 
 
+def split_name(file_name: str):
+    name_only, file_extension = os.path.splitext(file_name)
+    return name_only, file_extension
+
+
 def get_exension(file_name: str):
     _, file_extension = os.path.splitext(file_name)
     return file_extension
