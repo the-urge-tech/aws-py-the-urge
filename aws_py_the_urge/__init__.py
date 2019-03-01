1aac284a55de1df13770c3f541d371c3552db4e2
rachid <azi92rach@gmail.com>
2019-03-01 21:32:46 +1100

    update version to 1.0

--- aws_py_the_urge/__init__.py
+++ aws_py_the_urge/__init__.py
@@ -1,7 +1,7 @@
 """aws_py_the_urge - An opinionated, minimal cookiecutter template for Python packages"""
 
-__version__ = '0.2.0'
-__author__ = 'the urge <pierre.caserta@gmail.com>'
+__version__ = '1.0.0'
+__author__ = 'the urge <pierre.caserta@gmail.com>, <rachid@theurge.com>'
 __all__ = []
 
 import aws_py_the_urge.util.logger
