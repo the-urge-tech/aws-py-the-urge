7168d87d0316978b9cc034e17af314fd9745eaa4
rachid <azi92rach@gmail.com>
2019-03-01 11:07:12 +1100

    update tests

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,6 +1,7 @@
 import unittest
 import logging
-from datetime import date
+from datetime import date, datetime
+from aws_py_the_urge.lib.s3_manager.file_manager import FileS3
 from aws_py_the_urge.util.date import get_newest_file, path_date_extractor
 
 LOG = logging.getLogger(__name__)
@@ -21,7 +22,7 @@ class TestPathManager(unittest.TestCase):
         date_extr = path_date_extractor(
             "format=original/year=2018/month=1/day=11/xxxx/xxx/39744_3417821_mp.xml.gz"
         )
-        expected = date(2018, 1,11)
+        expected = date(2018, 1, 11)
         self.assertEqual(date_extr, expected)
 
         date_extr = path_date_extractor(
@@ -32,53 +33,47 @@ class TestPathManager(unittest.TestCase):
 
     def test_1(self):
         list_path = [
-            "format=original/retailer_code=au-gucci/year=2018/month=10/day=25/None",
-            "format=original/retailer_code=au-gucci/year=2018/month=10/day=4/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=10/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=11/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=12/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=13/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=14/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=15/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=16/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=17/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=18/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=19/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=20/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=22/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=23/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=24/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=25/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=26/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=29/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=30/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=8/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=9/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=1/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=19/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=2/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=20/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=3/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=4/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2018/month=12/day=5/39744_3417821_mp.xml.gz",
-            "format=original/retailer_code=au-gucci/year=2019/month=1/day=2/",
-            "format=original/retailer_code=au-gucci/year=2019/month=1/day=2/39744_3417821_mp.xml.gz"
+            FileS3(
+                key=
+                'type=fetched/retailer_code=au-gucci/year=2019/month=2/day=12/crawl_id=20190212202406__au-gucci/20190212202406__au-gucci--fetched_v2.0--i8m868o9.jl.gz',
+                last_modify=datetime(2019, 2, 12, 9, 24, 58),
+                meta={}),
+            FileS3(
+                key=
+                'type=fetched/retailer_code=au-gucci/year=2019/month=2/day=7/crawl_id=20190207151857__au-gucci/20190207151857__au-gucci--fetched_v2.0--clgdi084.jl.gz',
+                last_modify=datetime(2019, 2, 7, 4, 20, 34),
+                meta={}),
+            FileS3(
+                key=
+                'type=fetched/retailer_code=au-gucci/year=2019/month=2/day=8/crawl_id=20190207151857__au-gucci/20190207151857__au-gucci--fetched_v2.0--f7vimbre.jl.gz',
+                last_modify=datetime(2019, 2, 7, 4, 19, 51),
+                meta={})
         ]
 
         newest_file = get_newest_file(list_path, '.gz')
         LOG.debug("newest_file: {}".format(newest_file))
         self.assertEqual(
             newest_file,
-            "format=original/retailer_code=au-gucci/year=2019/month=1/day=2/39744_3417821_mp.xml.gz"
+            "type=fetched/retailer_code=au-gucci/year=2019/month=2/day=12/crawl_id=20190212202406__au-gucci/20190212202406__au-gucci--fetched_v2.0--i8m868o9.jl.gz"
         )
 
     def test_no_gz_files(self):
         list_path = [
-            "format=original/retailer_code=au-gucci/year=2018/month=10/day=25/None",
-            "format=original/retailer_code=au-gucci/year=2018/month=10/day=4/",
-            "format=original/retailer_code=au-gucci/year=2018/month=11/day=10/39744_3417821",
-            "format=original/retailer_code=au-gucci/year=2019/month=1/day=2/",
-            "format=original/retailer_code=au-gucci/year=2019/month=1/day=2/"
+            FileS3(
+                key=
+                'type=fetched/retailer_code=au-gucci/year=2019/month=2/day=12/crawl_id=20190212202406__au-gucci/None',
+                last_modify=datetime(2019, 2, 12, 9, 24, 58),
+                meta={}),
+            FileS3(
+                key=
+                'type=fetched/retailer_code=au-gucci/year=2019/month=2/day=7/crawl_id=20190207151857__au-gucci/20190207151857__au-gucci--fetched_v2.0--clgdi084.jl',
+                last_modify=datetime(2019, 2, 7, 4, 20, 34),
+                meta={}),
+            FileS3(
+                key=
+                'type=fetched/retailer_code=au-gucci/year=2019/month=2/day=8/crawl_id=20190207151857__au-gucci/',
+                last_modify=datetime(2019, 2, 7, 4, 19, 51),
+                meta={})
         ]
 
         newest_file = get_newest_file(list_path, '.gz')
