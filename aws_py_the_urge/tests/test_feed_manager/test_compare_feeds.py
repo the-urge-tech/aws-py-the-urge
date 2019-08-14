cc4c49425a23523a0ca6ef0a0d52f84d51b36bb5
casertap <pierre.caserta@gmail.com>
2019-08-14 13:47:34 +1000

    tests

--- aws_py_the_urge/tests/test_feed_manager/test_compare_feeds.py
+++ aws_py_the_urge/tests/test_feed_manager/test_compare_feeds.py
@@ -12,15 +12,18 @@ def moked_s3(*args, **kwargs):
 
 
 def moked_get_last_feed_content(*args, **kwargs):
-    return {b'test1'}
+    return {b"test1"}
 
 
 class TestComaringFeeds(unittest.TestCase):
-    @patch('aws_py_the_urge.lib.s3_manager.s3_parent.S3Parent.__init__',
-           side_effect=moked_s3())
     @patch(
-        'aws_py_the_urge.lib.feed_manager.FeedManager.get_last_feed_content',
-        side_effect=moked_get_last_feed_content())
+        "aws_py_the_urge.lib.s3_manager.s3_parent.S3Parent.__init__",
+        side_effect=moked_s3(),
+    )
+    @patch(
+        "aws_py_the_urge.lib.feed_manager.FeedManager.get_last_feed_content",
+        side_effect=moked_get_last_feed_content(),
+    )
     def test_comparing_feeds_different(self, mock1, mock2):
         retailer_code = "au-ssense"
         response_body = b"different text"
@@ -30,11 +33,14 @@ class TestComaringFeeds(unittest.TestCase):
         mock1.assert_called()
         mock2.assert_called()
 
-    @patch('aws_py_the_urge.lib.s3_manager.s3_parent.S3Parent.__init__',
-           side_effect=moked_s3())
     @patch(
-        'aws_py_the_urge.lib.feed_manager.FeedManager.get_last_feed_content',
-        side_effect=moked_get_last_feed_content())
+        "aws_py_the_urge.lib.s3_manager.s3_parent.S3Parent.__init__",
+        side_effect=moked_s3(),
+    )
+    @patch(
+        "aws_py_the_urge.lib.feed_manager.FeedManager.get_last_feed_content",
+        side_effect=moked_get_last_feed_content(),
+    )
     def test_comparing_feeds_equal(self, mock1, mock2):
         retailer_code = "au-ssense"
         response_body = b"test1"
@@ -44,20 +50,22 @@ class TestComaringFeeds(unittest.TestCase):
         mock1.assert_called()
         mock2.assert_called()
 
-    @patch('aws_py_the_urge.lib.s3_manager.s3_parent.S3Parent.__init__',
-           side_effect=moked_s3())
     @patch(
-        'aws_py_the_urge.lib.feed_manager.FeedManager.get_last_feed_content',
-        side_effect=moked_get_last_feed_content())
+        "aws_py_the_urge.lib.s3_manager.s3_parent.S3Parent.__init__",
+        side_effect=moked_s3(),
+    )
+    @patch(
+        "aws_py_the_urge.lib.feed_manager.FeedManager.get_last_feed_content",
+        side_effect=moked_get_last_feed_content(),
+    )
     def test_comparing_feeds_zip_equal(self, mock1, mock2):
         retailer_code = "au-ssense"
-        with open("tests/resources/example_zip_file.txt", 'rb') as f:
+        with open("aws_py_the_urge/tests/resources/example_zip_file.txt", "rb") as f:
             response_body = f.read()
 
-        result = FeedManager(retailer_code,
-                             "").is_equal_to_last(response_body,
-                                                  binary=False,
-                                                  file_extension=".zip")
+        result = FeedManager(retailer_code, "").is_equal_to_last(
+            response_body, binary=False, file_extension=".zip"
+        )
         LOG.debug("result test_comparing_feeds_equal = {}".format(result))
 
         self.assertTrue(result)
