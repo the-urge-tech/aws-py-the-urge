0380419e8ae4990037d20246dba24b69ff705c7f
Pierre Caserta <pierre.caserta@zip.co>
2021-09-08 10:57:20 +1000

    fix spidername vs crawlid extract

--- aws_py_the_urge/tests/test_tempfile.py
+++ aws_py_the_urge/tests/test_tempfile.py
@@ -1,7 +1,10 @@
 import unittest
 import logging
 from datetime import date, datetime
-from aws_py_the_urge.util.tempfile import tempfile_crawlid_extractor
+from aws_py_the_urge.util.tempfile import (
+    tempfile_spidername_extractor,
+    tempfile_crawlid_extractor,
+)
 
 LOG = logging.getLogger(__name__)
 
@@ -11,30 +14,58 @@ class TestTempfile(unittest.TestCase):
     def setUpClass(cls):
         super(TestTempfile, cls).setUpClass()
 
-    def test_tempfile_cralid_extractor1(self):
-        crawl_id = tempfile_crawlid_extractor(
+    def test_tempfile_spidername_extractor1(self):
+        crawl_id = tempfile_spidername_extractor(
             "20210526201725__enau-adidas--fetched_v2.0--_dh_oanh.jl.gz"
         )
         expected = "enau-adidas"
         self.assertEqual(crawl_id, expected)
 
-    def test_tempfile_cralid_extractor2(self):
-        crawl_id = tempfile_crawlid_extractor(
+    def test_tempfile_spidername_extractor2(self):
+        crawl_id = tempfile_spidername_extractor(
             "20210526201725__enau-adidas--business--fetched_v2.0--_dh_oanh.jl.gz"
         )
         expected = "enau-adidas--business"
         self.assertEqual(crawl_id, expected)
 
-    def test_tempfile_cralid_extractor3(self):
-        crawl_id = tempfile_crawlid_extractor(
+    def test_tempfile_spidername_extractor3(self):
+        crawl_id = tempfile_spidername_extractor(
             "/test/20210526201725__enau-adidas--business--fetched_v2.0--_dh_oanh.jl.gz"
         )
         expected = "enau-adidas--business"
         self.assertEqual(crawl_id, expected)
 
-    def test_tempfile_cralid_extractor4(self):
-        crawl_id = tempfile_crawlid_extractor(
+    def test_tempfile_spidername_extractor4(self):
+        crawl_id = tempfile_spidername_extractor(
             "/test/20210526201725__enau-adidas--business--fetched_v2.0--_dh_oanh"
         )
         expected = "enau-adidas--business"
+        self.assertEqual(crawl_id, expected)
+
+    def test_tempfile_crawlid_extractor1(self):
+        crawl_id = tempfile_crawlid_extractor(
+            "20210526201725__enau-adidas--fetched_v2.0--_dh_oanh.jl.gz"
+        )
+        expected = "20210526201725__enau-adidas"
+        self.assertEqual(crawl_id, expected)
+
+    def test_tempfile_crawlid_extractor2(self):
+        crawl_id = tempfile_crawlid_extractor(
+            "20210526201725__enau-adidas--business--fetched_v2.0--_dh_oanh.jl.gz"
+        )
+        expected = "20210526201725__enau-adidas--business"
+        self.assertEqual(crawl_id, expected)
+
+    def test_tempfile_crawlid_extractor3(self):
+        crawl_id = tempfile_crawlid_extractor(
+            "/test/20210526201725__enau-adidas--business--fetched_v2.0--_dh_oanh.jl.gz"
+        )
+        expected = "20210526201725__enau-adidas--business"
+        self.assertEqual(crawl_id, expected)
+
+    def test_tempfile_crawlid_extractor4(self):
+        crawl_id = tempfile_crawlid_extractor(
+            "/test/20210526201725__enau-adidas--business--fetched_v2.0--_dh_oanh"
+        )
+        expected = "20210526201725__enau-adidas--business"
         self.assertEqual(crawl_id, expected)
--- aws_py_the_urge/util/tempfile.py
+++ aws_py_the_urge/util/tempfile.py
@@ -8,6 +8,21 @@ def tempfile_crawlid_extractor(tempfile):
     if "/" in tempfile:
         tempfile = tempfile.split("/")[-1]
 
+    matches = re.search(r"(.*)--.*--(.*?)\..*", tempfile)
+    # if no extension
+    if "." not in tempfile:
+        matches = re.search(r"(.*)--.*--(.*)", tempfile)
+
+    return matches.group(1)
+
+
+def tempfile_spidername_extractor(tempfile):
+    # remove _v2.0 we dont want the .
+    tempfile = re.sub(r"_v.\..", "", tempfile)
+    # keep only the filename
+    if "/" in tempfile:
+        tempfile = tempfile.split("/")[-1]
+
     matches = re.search(r".*__(.*)--.*--(.*?)\..*", tempfile)
     # if no extension
     if "." not in tempfile:
